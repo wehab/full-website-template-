5a73bc78a8821f91c916e8536b2fae5c23a77002
Wehab Zaidi <wehabraza72@gmail.com>
2021-05-18 17:13:54 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+  // Get current path and find target link
+  var path = window.location.pathname.split("/").pop();
+  
+  // Account for home page with empty path
+  if ( path == '' ) {
+    path = 'index.html';
+  }
+      
+  var target = $('nav a[href="'+path+'"]');
+  // Add active class to target link
+  target.addClass('active');
+});
